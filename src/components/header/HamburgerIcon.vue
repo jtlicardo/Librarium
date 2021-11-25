69a83e04bd8a837f51381e517e6cd10264b4b220
Josip Tomo Licardo <licardo.josip@gmail.com>
2021-11-25 08:44:25 +0100

    Hamburger icon component

---
+++ src/components/header/HamburgerIcon.vue
@@ -0,0 +1,71 @@
+<template>
+  <svg
+    class="ham ham6"
+    viewBox="0 0 100 100"
+    width="50"
+    :class="{ active: sidebarOpen }"
+    id="hamIcon"
+  >
+    <path
+      class="line top"
+      d="m 30,33 h 40 c 13.100415,0 14.380204,31.80258 6.899646,33.421777 -24.612039,5.327373 9.016154,-52.337577 -12.75751,-30.563913 l -28.284272,28.284272"
+    />
+    <path
+      class="line middle"
+      d="m 70,50 c 0,0 -32.213436,0 -40,0 -7.786564,0 -6.428571,-4.640244 -6.428571,-8.571429 0,-5.895471 6.073743,-11.783399 12.286435,-5.570707 6.212692,6.212692 28.284272,28.284272 28.284272,28.284272"
+    />
+    <path
+      class="line bottom"
+      d="m 69.575405,67.073826 h -40 c -13.100415,0 -14.380204,-31.80258 -6.899646,-33.421777 24.612039,-5.327373 -9.016154,52.337577 12.75751,30.563913 l 28.284272,-28.284272"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  computed: {
+    sidebarOpen() {
+      return this.$store.state.sidebarOpen
+    },
+  },
+}
+</script>
+
+<style>
+.ham {
+  cursor: pointer;
+  -webkit-tap-highlight-color: transparent;
+  transition: transform 400ms;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+.line {
+  fill: none;
+  transition: stroke-dasharray 400ms, stroke-dashoffset 400ms;
+  stroke: #fff;
+  stroke-width: 5.5;
+  stroke-linecap: round;
+}
+
+.ham6 .top {
+  stroke-dasharray: 40 172;
+}
+.ham6 .middle {
+  stroke-dasharray: 40 111;
+}
+.ham6 .bottom {
+  stroke-dasharray: 40 172;
+}
+.ham6.active .top {
+  stroke-dashoffset: -132px;
+}
+.ham6.active .middle {
+  stroke-dashoffset: -71px;
+}
+.ham6.active .bottom {
+  stroke-dashoffset: -132px;
+}
+</style>
--- src/components/header/TheHeader.vue
+++ src/components/header/TheHeader.vue
@@ -6,27 +6,7 @@
     app
     class="d-flex justify-space-between"
   >
-    <svg
-      class="ham ham6"
-      viewBox="0 0 100 100"
-      width="50"
-      :class="{ active: sidebarOpen }"
-      id="hamIcon"
-      @click="toggleSidebar"
-    >
-      <path
-        class="line top"
-        d="m 30,33 h 40 c 13.100415,0 14.380204,31.80258 6.899646,33.421777 -24.612039,5.327373 9.016154,-52.337577 -12.75751,-30.563913 l -28.284272,28.284272"
-      />
-      <path
-        class="line middle"
-        d="m 70,50 c 0,0 -32.213436,0 -40,0 -7.786564,0 -6.428571,-4.640244 -6.428571,-8.571429 0,-5.895471 6.073743,-11.783399 12.286435,-5.570707 6.212692,6.212692 28.284272,28.284272 28.284272,28.284272"
-      />
-      <path
-        class="line bottom"
-        d="m 69.575405,67.073826 h -40 c -13.100415,0 -14.380204,-31.80258 -6.899646,-33.421777 24.612039,-5.327373 -9.016154,52.337577 12.75751,30.563913 l 28.284272,-28.284272"
-      />
-    </svg>
+    <hamburger-icon @click.native="toggleSidebar"></hamburger-icon>
 
     <v-img
       src="../../assets/logo.png"
@@ -34,8 +14,6 @@
       max-height="40px"
       :max-width="imageWidth"
     ></v-img>
-
-    <!-- <p>User icon</p> -->
 
     <v-menu offset-y>
       <template v-slot:activator="{ on, attrs }">
@@ -56,16 +34,13 @@
 </template>
 
 <script>
+import HamburgerIcon from "./HamburgerIcon.vue"
+
 export default {
-  methods: {
-    toggleSidebar() {
-      this.$store.commit("toggleSidebar")
-    },
+  components: {
+    HamburgerIcon,
   },
   computed: {
-    sidebarOpen() {
-      return this.$store.state.sidebarOpen
-    },
     imageWidth() {
       switch (this.$vuetify.breakpoint.name) {
         case "xs":
@@ -79,6 +54,11 @@ export default {
         case "xl":
           return "200px"
       }
+    },
+  },
+  methods: {
+    toggleSidebar() {
+      this.$store.commit("toggleSidebar")
     },
   },
   watch: {
@@ -95,42 +75,5 @@ export default {
 .v-toolbar__content {
   width: 100%;
   justify-content: space-between;
-}
-
-.ham {
-  cursor: pointer;
-  -webkit-tap-highlight-color: transparent;
-  transition: transform 400ms;
-  -moz-user-select: none;
-  -webkit-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-}
-
-.line {
-  fill: none;
-  transition: stroke-dasharray 400ms, stroke-dashoffset 400ms;
-  stroke: #fff;
-  stroke-width: 5.5;
-  stroke-linecap: round;
-}
-
-.ham6 .top {
-  stroke-dasharray: 40 172;
-}
-.ham6 .middle {
-  stroke-dasharray: 40 111;
-}
-.ham6 .bottom {
-  stroke-dasharray: 40 172;
-}
-.ham6.active .top {
-  stroke-dashoffset: -132px;
-}
-.ham6.active .middle {
-  stroke-dashoffset: -71px;
-}
-.ham6.active .bottom {
-  stroke-dashoffset: -132px;
 }
 </style>
