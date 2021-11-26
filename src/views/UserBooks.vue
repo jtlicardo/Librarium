ed00bad189259bb2e934a014634caa152b48f168
Josip Tomo Licardo <licardo.josip@gmail.com>
2021-11-26 19:31:28 +0100

    Update UserBooks.vue

--- src/views/UserBooks.vue
+++ src/views/UserBooks.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    <h1>User Books</h1>
+    <h1>Search for a book...</h1>
+    <v-form>
+      <v-container>
+        <v-row>
+          <v-col cols="5">
+            <v-text-field v-model="firstname" label="Title"></v-text-field>
+
+            <v-text-field v-model="lastname" label="Author"></v-text-field>
+
+            <v-text-field v-model="email" label="Genre"></v-text-field>
+          </v-col>
+          <v-col cols="2"></v-col>
+          <v-col cols="5">Recently added books</v-col>
+        </v-row>
+      </v-container>
+    </v-form>
   </div>
 </template>
 
