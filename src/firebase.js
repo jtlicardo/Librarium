90f13116e4b7286611559e6a2fa2b41d3ac30137
Josip Tomo Licardo <licardo.josip@gmail.com>
2021-12-12 10:45:10 +0100

    Added Firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDmpWucdj9MuwM5mvjA5_TKMCFlsUXUGpg",
+  authDomain: "librarium-11060.firebaseapp.com",
+  projectId: "librarium-11060",
+  storageBucket: "librarium-11060.appspot.com",
+  messagingSenderId: "599810153981",
+  appId: "1:599810153981:web:3f37c90da71ecab6580d94",
+}
+
+const app = initializeApp(firebaseConfig)
