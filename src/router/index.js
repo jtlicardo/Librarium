cdec2e62a1e30c03d408ceeb9f2ca307ff0e5a96
Josip Tomo Licardo <licardo.josip@gmail.com>
2021-12-19 13:11:03 +0100

    Admin views

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import Vue from "vue"
 import VueRouter from "vue-router"
-import UserBooks from "@/views/UserBooks.vue"
-import UserReservations from "@/views/UserReservations.vue"
-import UserLoans from "@/views/UserLoans.vue"
-import BookSearch from "@/views/BookSearch.vue"
-import BookDetails from "@/views/BookDetails.vue"
-import UserAuth from "@/views/UserAuth.vue"
+import UserBooks from "@/views/user/UserBooks.vue"
+import UserReservations from "@/views/user/UserReservations.vue"
+import UserLoans from "@/views/user/UserLoans.vue"
+import BookSearch from "@/views/user/BookSearch.vue"
+import BookDetails from "@/views/user/BookDetails.vue"
+import AuthPage from "@/views/AuthPage.vue"
+import AdminBooks from "@/views/admin/AdminBooks.vue"
+import AdminLoans from "@/views/admin/AdminLoans.vue"
+import AdminReservations from "@/views/admin/AdminReservations.vue"
+import AdminRequests from "@/views/admin/AdminRequests.vue"
+import AdminUsers from "@/views/admin/AdminUsers.vue"
 
 Vue.use(VueRouter)
 
@@ -13,7 +18,7 @@ const routes = [
   {
     name: "Authentication",
     path: "/auth",
-    component: UserAuth,
+    component: AuthPage,
     meta: {
       needsUnauth: true,
       title: "Authentication",
@@ -67,6 +72,56 @@ const routes = [
     meta: {
       needsAuth: true,
       title: "Book Details",
+    },
+  },
+  {
+    name: "Admin Books",
+    path: "/adminbooks",
+    component: AdminBooks,
+    meta: {
+      needsAuth: true,
+      needsAdmin: true,
+      title: "Books",
+    },
+  },
+  {
+    name: "Admin Loans",
+    path: "/adminloans",
+    component: AdminLoans,
+    meta: {
+      needsAuth: true,
+      needsAdmin: true,
+      title: "Loans",
+    },
+  },
+  {
+    name: "Admin Requests",
+    path: "/adminrequests",
+    component: AdminRequests,
+    meta: {
+      needsAuth: true,
+      needsAdmin: true,
+      title: "Requests",
+    },
+  },
+  {
+    name: "Admin Reservations",
+    path: "/adminreservations",
+    component: AdminReservations,
+    meta: {
+      needsAuth: true,
+      needsAdmin: true,
+      title: "Reservations",
+    },
+  },
+  {
+    name: "Admin Users",
+    path: "/adminusers",
+    component: AdminUsers,
+    meta: {
+      needsAuth: true,
+      needsAdmin: true,
+      title: "Users",
     },
   },
 ]
---
+++ src/views/admin/AdminBooks.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>Admin books</p>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
