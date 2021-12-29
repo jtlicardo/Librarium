9f500b52660ee5122d1dcb9c9856cd4ed4af849b
Josip Tomo Licardo <licardo.josip@gmail.com>
2021-12-29 14:20:55 +0100

    Update BookSearch.vue

--- src/views/user/BookSearch.vue
+++ src/views/user/BookSearch.vue
@@ -1,16 +1,17 @@
 <template>
   <v-container class="mt-md-10">
     <v-row>
-      <v-col cols="12" md="2"></v-col>
+      <v-col cols="12" md="1"></v-col>
       <v-col cols="12" md="10">
         <h1 class="mb-5">Search results</h1>
         <filtered-books />
         <p class="mt-15">Can't find the book you're looking for?</p>
         <p>
-          <u>Send a request</u>
+          <a>Send a request</a>
           to add a book to the library's collection.
         </p>
       </v-col>
+      <v-col cols="12" md="1"></v-col>
     </v-row>
   </v-container>
 </template>
@@ -28,5 +29,15 @@ export default {
 <style scoped>
 h1 {
   font-weight: 400;
+}
+
+p,
+p a {
+  color: gray;
+}
+
+p a {
+  text-decoration: underline;
+  font-weight: 500;
 }
 </style>
