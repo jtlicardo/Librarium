7f521efa9d3b7160e9fb7e4c8cba2b29f4345352
Josip Tomo Licardo <licardo.josip@gmail.com>
2021-12-29 17:53:20 +0100

    Displaying book requests to admin

---
+++ src/components/admin/BookRequests.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="requests"
+    hide-default-footer
+    class="elevation-1"
+    :loading="loading"
+  >
+    <template v-slot:[`item.delete`]>
+      <v-btn class="mx-2" color="red darken-1" fab x-small elevation="1">
+        <v-icon color="white">mdi-trash-can-outline</v-icon>
+      </v-btn>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+import { db, collection, getDocs } from "@/firebase.js"
+
+export default {
+  data() {
+    return {
+      loading: false,
+      headers: [
+        {
+          text: "TITLE",
+          sortable: false,
+          value: "title",
+          align: "center",
+        },
+        {
+          text: "AUTHOR",
+          value: "author",
+          sortable: false,
+          align: "center",
+        },
+      ],
+      requests: [],
+    }
+  },
+  methods: {
+    async getBookRequests() {
+      const querySnapshot = await getDocs(collection(db, "bookRequests"))
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, " => ", doc.data())
+        this.requests.push(doc.data())
+      })
+    },
+  },
+  created() {
+    this.getBookRequests()
+  },
+}
+</script>
+
+<style scoped></style>
--- src/views/admin/AdminRequests.vue
+++ src/views/admin/AdminRequests.vue
@@ -1,9 +1,34 @@
 <template>
-    <p>Admin requests</p>
+  <v-container class="mt-md-10">
+    <v-row>
+      <v-col cols="12" md="1" lg="1" xl="1"></v-col>
+      <v-col cols="12" md="9" lg="7" xl="5">
+        <h1 class="mb-10">Books users have requested</h1>
+        <book-requests></book-requests>
+      </v-col>
+      <v-col cols="12" md="2" lg="4" xl="6"></v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" md="1"></v-col>
+      <v-col cols="12" md="10">
+        <h1 class="my-10">Loan extension requests</h1>
+      </v-col>
+      <v-col cols="12" md="1"></v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {}
+import BookRequests from "@/components/admin/BookRequests.vue"
+export default {
+  components: {
+    BookRequests,
+  },
+}
 </script>
 
-<style></style>
+<style scoped>
+h1 {
+  font-weight: 400;
+}
+</style>
