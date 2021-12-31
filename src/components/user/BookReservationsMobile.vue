c0bec4563e3482d6185ca34a0c138c5c74cfe731
Josip Tomo Licardo <licardo.josip@gmail.com>
2021-12-31 16:28:42 +0100

    Update BookReservationsMobile.vue

--- src/components/user/BookReservationsMobile.vue
+++ src/components/user/BookReservationsMobile.vue
@@ -3,7 +3,7 @@
     :headers="headerTitles"
     :items="reservations"
     :single-expand="singleExpand"
-    item-key="name"
+    item-key="title"
     show-expand
     hide-default-header
     hide-default-footer
@@ -15,7 +15,7 @@
         <b class="text-h6">RESERVATIONS DETAILS</b>
         <p class="my-0">
           <b>Book title:</b>
-          {{ item.name }}
+          {{ item.title }}
         </p>
         <p class="my-0">
           <b>Author:</b>
@@ -33,13 +33,32 @@
           <b>End date:</b>
           {{ item.endDate }}
         </p>
-        <v-btn class="mt-3 white--text" color="red darken-1">CANCEL RESERVATION</v-btn>
+        <v-btn
+          class="mt-3 white--text"
+          color="red darken-1"
+          @click="cancelReservation(item)"
+        >
+          CANCEL RESERVATION
+        </v-btn>
       </td>
     </template>
   </v-data-table>
 </template>
 
 <script>
+import {
+  db,
+  collection,
+  query,
+  where,
+  getDocs,
+  deleteDoc,
+  updateDoc,
+  arrayUnion,
+  arrayRemove,
+  doc,
+} from "@/firebase.js"
+
 export default {
   data() {
     return {
@@ -49,30 +68,109 @@ export default {
           text: "",
           align: "start",
           sortable: false,
-          value: "name",
+          value: "title",
         },
         {
           text: "",
           value: "data-table-expand",
         },
       ],
-      reservations: [
-        {
-          name: "Crime and Punishment",
-          author: "Fyodor Dostoevsky",
-          inventoryNumber: "26008",
-          startDate: "16.11.2021.",
-          endDate: "19.11.2021.",
-        },
-        {
-          name: "Sapiens - A Brief History of Humankind",
-          author: "Yuval Noah Harari",
-          inventoryNumber: "57961",
-          startDate: "15.11.2021.",
-          endDate: "18.11.2021.",
-        },
-      ],
+      firebaseReservations: [],
+      reservations: [],
     }
+  },
+  methods: {
+    milisecondsToDate(miliseconds) {
+      let date = new Date(miliseconds)
+      const day = date.getDate()
+      const month = date.getMonth() + 1
+      const year = date.getFullYear()
+      const result = day + "." + month + "." + year + "."
+      return result
+    },
+    async getUserReservations() {
+      this.loading = true
+      const userId = localStorage.getItem("userId")
+      const reservationsRef = collection(db, "reservations")
+      const q = query(reservationsRef, where("userId", "==", userId))
+      const querySnapshot = await getDocs(q)
+      querySnapshot.forEach((doc) => {
+        this.firebaseReservations.push({
+          reservationId: doc.id,
+          author: doc.data().author,
+          title: doc.data().title,
+          bookId: doc.data().bookId,
+          copyInvNumber: doc.data().copyInvNumber,
+          userId: doc.data().userId,
+          start_time: doc.data().start_time,
+          end_time: doc.data().end_time,
+        })
+        const startDate = this.milisecondsToDate(doc.data().start_time)
+        const endDate = this.milisecondsToDate(doc.data().end_time)
+        this.reservations.push({
+          title: doc.data().title,
+          author: doc.data().author,
+          inventoryNumber: doc.data().copyInvNumber,
+          startDate,
+          endDate,
+        })
+      })
+      this.loading = false
+    },
+    async cancelReservation(item) {
+      const selectedReservation = this.firebaseReservations.find(
+        (el) =>
+          el.author === item.author &&
+          el.title === item.title &&
+          el.copyInvNumber === item.inventoryNumber
+      )
+      console.log(selectedReservation)
+      try {
+        const booksRef = doc(db, "books", selectedReservation.bookId)
+        await updateDoc(booksRef, {
+          copies: arrayRemove({
+            inventoryNumber: selectedReservation.copyInvNumber,
+            status: "Reserved",
+          }),
+        })
+        await updateDoc(booksRef, {
+          copies: arrayUnion({
+            inventoryNumber: selectedReservation.copyInvNumber,
+            status: "Available",
+          }),
+        })
+        const usersRef = collection(db, "users")
+        const q = query(usersRef, where("uid", "==", selectedReservation.userId))
+        const querySnapshot = await getDocs(q)
+        let selectedUser = ""
+        querySnapshot.forEach((doc) => {
+          selectedUser = doc.id
+        })
+        const users = doc(db, "users", selectedUser)
+        await updateDoc(users, {
+          reservations: arrayRemove(selectedReservation.reservationId),
+        })
+        await deleteDoc(doc(db, "reservations", selectedReservation.reservationId))
+        this.$store.dispatch("displaySnackbar", {
+          text: "Reservation canceled!",
+          isActive: true,
+        })
+        this.reservations = []
+        await this.getUserReservations()
+      } catch (e) {
+        console.log("Error: ", e)
+        this.$store.dispatch("displayBaseDialog", {
+          text: e.toString(),
+          title: "Error! Please try again later.",
+          color: "red",
+          loading: false,
+          active: true,
+        })
+      }
+    },
+  },
+  async created() {
+    await this.getUserReservations()
   },
 }
 </script>
