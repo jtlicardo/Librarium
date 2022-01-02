9f48a23ea96e3c6b7acb5283bd1b043bdae54007
Josip Tomo Licardo <licardo.josip@gmail.com>
2022-01-02 08:08:44 +0100

    Loans admin view basic layout

---
+++ src/components/admin/BookLoans.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="loans"
+    hide-default-footer
+    class="elevation-1"
+    :loading="loading"
+  >
+    <!-- <template v-slot:[`item.edit`]="{ item }">
+      <v-icon color="red" @click="editLoan(item)">mdi-trash-can-outline</v-icon>
+    </template> -->
+  </v-data-table>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- src/views/admin/AdminLoans.vue
+++ src/views/admin/AdminLoans.vue
@@ -1,9 +1,29 @@
 <template>
-  <p>Admin loans</p>
+  <v-container class="mt-md-10">
+    <v-row>
+      <v-col cols="12" md="1"></v-col>
+      <v-col cols="12" md="10">
+        <h1 class="mb-10">All loans</h1>
+        <book-loans />
+        <v-btn class="my-14" color="yellow darken-1">Add loan</v-btn>
+      </v-col>
+      <v-col cols="12" md="1"></v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {}
+import BookLoans from "@/components/admin/BookLoans.vue"
+
+export default {
+  components: {
+    BookLoans,
+  },
+}
 </script>
 
-<style></style>
+<style scoped>
+h1 {
+  font-weight: 400;
+}
+</style>
