a3f775849da545ed52a6d9af5071f1b7f567e37f
Josip Tomo Licardo <licardo.josip@gmail.com>
2022-01-02 15:40:25 +0100

    Displaying all loans to admin

--- src/components/admin/BookLoans.vue
+++ src/components/admin/BookLoans.vue
@@ -6,13 +6,21 @@
     class="elevation-1"
     :loading="loading"
   >
-    <!-- <template v-slot:[`item.edit`]="{ item }">
-      <v-icon color="red" @click="editLoan(item)">mdi-trash-can-outline</v-icon>
-    </template> -->
+    <template v-slot:[`item.copy`]="{ item }">
+      {{ item.title }}
+      <br />
+      {{ item.author }}
+      <br />
+      {{ item.copyInvNumber }}
+    </template>
+    <template v-slot:[`item.edit`]="{ item }">
+      <v-icon color="primary" @click="editLoan(item)">mdi-circle-edit-outline</v-icon>
+    </template>
   </v-data-table>
 </template>
 
 <script>
+import { db, collection, getDocs, query, where } from "@/firebase.js"
 export default {
   data() {
     return {
@@ -20,14 +28,109 @@ export default {
       loans: [],
       headers: [
         {
-          text: "BOOK TITLE",
-          value: "title",
+          text: "BOOK COPY",
+          value: "copy",
           sortable: false,
           align: "center",
-          filterable: true,
+        },
+        {
+          text: "USER",
+          value: "user",
+          sortable: false,
+          align: "center",
+        },
+        {
+          text: "ISSUE DATE",
+          value: "issue_date",
+          sortable: false,
+          align: "center",
+        },
+        {
+          text: "DUE DATE",
+          value: "due_date",
+          sortable: false,
+          align: "center",
+        },
+        {
+          text: "RETURN DATE",
+          value: "return_date",
+          sortable: false,
+          align: "center",
+        },
+        {
+          text: "LOAN STATUS",
+          value: "loan_status",
+          sortable: false,
+          align: "center",
+        },
+        {
+          text: "EDIT",
+          value: "edit",
+          sortable: false,
+          align: "center",
         },
       ],
     }
+  },
+  methods: {
+    milisecondsToDate(miliseconds) {
+      let date = new Date(miliseconds)
+      const day = date.getDate()
+      const month = date.getMonth() + 1
+      const year = date.getFullYear()
+      const result = day + "." + month + "." + year + "."
+      return result
+    },
+    async getAllLoans() {
+      this.loading = true
+      const querySnapshot = await getDocs(collection(db, "loans"))
+      querySnapshot.forEach((doc) => {
+        const issue_date = this.milisecondsToDate(doc.data().issue_time)
+        const due_date = this.milisecondsToDate(doc.data().due_time)
+        let return_date = ""
+        if (doc.data().return_time !== null) {
+          return_date = this.milisecondsToDate(doc.data().return_time)
+        }
+        this.loans.push({
+          firebaseLoanId: doc.id,
+          firebaseBookId: doc.data().bookId,
+          firebaseIssueTime: doc.data().issue_time,
+          firebaseDueTime: doc.data().due_time,
+          firebaseReturnTime: doc.data().return_time,
+          firebaseExtensionRequested: doc.data().extensionRequested,
+          author: doc.data().author,
+          copyInvNumber: doc.data().copyInvNumber,
+          loan_status: doc.data().loan_status,
+          title: doc.data().title,
+          userId: doc.data().userId,
+          issue_date,
+          due_date,
+          return_date,
+        })
+      })
+      await this.changeAllIdsToEmails(this.loans)
+      this.loading = false
+    },
+    async userIdToEmail(userId) {
+      const usersRef = collection(db, "users")
+      const q = query(usersRef, where("uid", "==", userId))
+      const querySnapshot = await getDocs(q)
+      let email = ""
+      querySnapshot.forEach((doc) => {
+        email = doc.data().email
+      })
+      return email
+    },
+    async changeAllIdsToEmails(loans) {
+      for (let loan of loans) {
+        const userId = loan.userId
+        const email = await this.userIdToEmail(userId)
+        loan.user = email
+      }
+    },
+  },
+  async created() {
+    await this.getAllLoans()
   },
 }
 </script>
