67d8f01e9f5173485fed6cffc7b6ce51ed139af7
Josip Tomo Licardo <licardo.josip@gmail.com>
2022-01-09 18:12:03 +0100

    Loans filter

--- src/components/admin/BookLoans.vue
+++ src/components/admin/BookLoans.vue
@@ -1,58 +1,70 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="loans"
-    hide-default-footer
-    class="elevation-1"
-    :loading="loading"
-  >
-    <template v-slot:[`item.copy`]="{ item }">
-      {{ item.title }}
-      <br />
-      {{ item.author }}
-      <br />
-      {{ item.copyInvNumber }}
-    </template>
-    <template v-slot:[`item.loan_status`]="{ item }">
-      <loan-status :type="item.loan_status"></loan-status>
-    </template>
-    <template v-slot:[`item.edit`]="{ item }">
-      <v-icon color="primary" @click="editLoan(item)" v-if="!isMobile">
-        mdi-circle-edit-outline
-      </v-icon>
-      <v-btn color="primary" @click="editLoan(item)" v-else class="edit-button">
-        Edit
-      </v-btn>
-    </template>
-    <template v-slot:[`item.accept`]="{ item }">
-      <v-icon color="success" @click="acceptRequest(item)" v-if="!isMobile">
-        mdi-check-outline
-      </v-icon>
-      <div class="text-center" v-else>
-        <v-btn
-          color="green white--text"
-          class="mx-3 my-5"
-          width="90px"
-          @click="acceptRequest(item)"
-        >
-          Accept
+  <v-card>
+    <v-card-title>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Filter (by book copy or user)"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="loans"
+      hide-default-footer
+      class="elevation-1"
+      :loading="loading"
+      :search="search"
+    >
+      <template v-slot:[`item.copy`]="{ item }">
+        {{ item.title }}
+        <br />
+        {{ item.author }}
+        <br />
+        {{ item.copyInvNumber }}
+      </template>
+      <template v-slot:[`item.loan_status`]="{ item }">
+        <loan-status :type="item.loan_status"></loan-status>
+      </template>
+      <template v-slot:[`item.edit`]="{ item }">
+        <v-icon color="primary" @click="editLoan(item)" v-if="!isMobile">
+          mdi-circle-edit-outline
+        </v-icon>
+        <v-btn color="primary" @click="editLoan(item)" v-else class="edit-button">
+          Edit
         </v-btn>
-        <v-btn
-          color="red white--text"
-          class="mx-3 my-5"
-          width="90px"
-          @click="denyRequest(item)"
-        >
-          Deny
-        </v-btn>
-      </div>
-    </template>
-    <template v-slot:[`item.deny`]="{ item }">
-      <v-icon color="red" @click="denyRequest(item)" v-if="!isMobile">
-        mdi-close-outline
-      </v-icon>
-    </template>
-  </v-data-table>
+      </template>
+      <template v-slot:[`item.accept`]="{ item }">
+        <v-icon color="success" @click="acceptRequest(item)" v-if="!isMobile">
+          mdi-check-outline
+        </v-icon>
+        <div class="text-center" v-else>
+          <v-btn
+            color="green white--text"
+            class="mx-3 my-5"
+            width="90px"
+            @click="acceptRequest(item)"
+          >
+            Accept
+          </v-btn>
+          <v-btn
+            color="red white--text"
+            class="mx-3 my-5"
+            width="90px"
+            @click="denyRequest(item)"
+          >
+            Deny
+          </v-btn>
+        </div>
+      </template>
+      <template v-slot:[`item.deny`]="{ item }">
+        <v-icon color="red" @click="denyRequest(item)" v-if="!isMobile">
+          mdi-close-outline
+        </v-icon>
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
 
 <script>
@@ -81,6 +93,7 @@ export default {
   data() {
     return {
       loading: false,
+      search: "",
       loans: [],
     }
   },
@@ -100,36 +113,63 @@ export default {
           value: "copy",
           sortable: false,
           align: "center",
+          filterable: false,
+        },
+        {
+          text: "TITLE",
+          value: "title",
+          sortable: false,
+          align: " d-none",
+          filterable: true,
+        },
+        {
+          text: "AUTHOR",
+          value: "author",
+          sortable: false,
+          align: " d-none",
+          filterable: true,
+        },
+        {
+          text: "INVNUM",
+          value: "copyInvNumber",
+          sortable: false,
+          align: " d-none",
+          filterable: true,
         },
         {
           text: "USER",
           value: "user",
           sortable: false,
           align: "center",
+          filterable: true,
         },
         {
           text: "ISSUE DATE",
           value: "issue_date",
           sortable: false,
           align: "center",
+          filterable: false,
         },
         {
           text: "DUE DATE",
           value: "due_date",
           sortable: false,
           align: "center",
+          filterable: false,
         },
         {
           text: "RETURN DATE",
           value: "return_date",
           sortable: false,
           align: "center",
+          filterable: false,
         },
         {
           text: "LOAN STATUS",
           value: "loan_status",
           sortable: false,
           align: "center",
+          filterable: false,
         },
       ]
       if (!this.requests && !this.isMobile) {
@@ -354,5 +394,26 @@ export default {
   > .v-data-table__mobile-row:first-child
   > .v-data-table__mobile-row__cell {
   padding-top: 10px;
+}
+
+.v-data-table
+  >>> .v-data-table__wrapper
+  > table
+  > tbody
+  > .v-data-table__mobile-table-row
+  > .v-data-table__mobile-row:nth-of-type(2),
+.v-data-table
+  >>> .v-data-table__wrapper
+  > table
+  > tbody
+  > .v-data-table__mobile-table-row
+  > .v-data-table__mobile-row:nth-of-type(3),
+.v-data-table
+  >>> .v-data-table__wrapper
+  > table
+  > tbody
+  > .v-data-table__mobile-table-row
+  > .v-data-table__mobile-row:nth-of-type(4) {
+  display: none;
 }
 </style>
